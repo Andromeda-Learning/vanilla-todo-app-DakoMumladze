93dd34196ed1571015004a0383d9e8e36710fb61
Dako <dako.mumladze@geolab.edu.ge>
2020-03-07 18:53:50 +0400

    created delete item functionality (#3)

--- main.js
+++ main.js
@@ -8,15 +8,20 @@ function addItem(event) {
 }
 
 function createListItem() {
-    if(inputText.value.trim() !== "") {
+    if (inputText.value.trim() !== "") {
         const inputTextTrimmed = inputText.value.trim()
         const createdItem = document.createElement('li')
+        const deleteBtn = document.createElement('button')
+        const deleteBtnText = document.createTextNode('delete')
         const createdItemText = document.createTextNode(inputTextTrimmed)
         createdItem.appendChild(createdItemText)
+        deleteBtn.appendChild(deleteBtnText)
+        createdItem.appendChild(deleteBtn)
         itemsCont.appendChild(createdItem)
+        document.body.appendChild(itemsCont)
+        deleteBtn.addEventListener('click',deleteListItem)
         inputText.classList.remove('error')
-    }
-    else {
+    } else {
         inputText.classList.add('error')
     }
 }
@@ -24,3 +29,10 @@ function createListItem() {
 function clearInput() {
     inputText.value = ""
 }
+
+
+function deleteListItem() {
+    itemsCont.removeChild(this.parentNode)
+}
+
+
